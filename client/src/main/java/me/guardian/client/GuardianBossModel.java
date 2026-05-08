0d63e45594948cca252fd08ee239a051ba22a108
styy_ <130603602+sty892@users.noreply.github.com>
2026-05-08 21:12:32 +0200

    fix(client): use geckolib5 model method names

--- client/src/main/java/me/guardian/client/GuardianBossModel.java
+++ client/src/main/java/me/guardian/client/GuardianBossModel.java
@@ -5,35 +5,42 @@ import me.guardian.ModState;
 import net.minecraft.resources.Identifier;
 import net.minecraft.world.entity.Mob;
 import software.bernie.geckolib.animatable.GeoEntity;
-import software.bernie.geckolib.model.DefaultedEntityGeoModel;
+import software.bernie.geckolib.model.GeoModel;
 import software.bernie.geckolib.renderer.base.GeoRenderState;
 
-public final class GuardianBossModel<T extends Mob & GeoEntity> extends DefaultedEntityGeoModel<T> {
-    private static final Identifier FALLBACK_ASSET = Identifier.fromNamespaceAndPath(GuardianMod.MOD_ID, "boss_fallback");
-
-    private final Identifier bossAsset;
+public final class GuardianBossModel<T extends Mob & GeoEntity> extends GeoModel<T> {
+    private final Identifier bossModel;
+    private final Identifier bossTexture;
+    private final Identifier bossAnimation;
+    private final Identifier fallbackModel;
+    private final Identifier fallbackTexture;
+    private final Identifier fallbackAnimation;
 
     public GuardianBossModel(String bossAssetName) {
-        super(Identifier.fromNamespaceAndPath(GuardianMod.MOD_ID, bossAssetName));
-        this.bossAsset = Identifier.fromNamespaceAndPath(GuardianMod.MOD_ID, bossAssetName);
+        this.bossModel = asset("geckolib/models/entity/" + bossAssetName + ".geo.json");
+        this.bossTexture = asset("textures/entity/" + bossAssetName + ".png");
+        this.bossAnimation = asset("geckolib/animations/entity/" + bossAssetName + ".animation.json");
+        this.fallbackModel = asset("geckolib/models/entity/boss_fallback.geo.json");
+        this.fallbackTexture = asset("textures/entity/boss_fallback.png");
+        this.fallbackAnimation = asset("geckolib/animations/entity/boss_fallback.animation.json");
     }
 
     @Override
-    public Identifier getModelResource(GeoRenderState renderState) {
-        return buildFormattedModelPath(selectedAsset());
+    public Identifier getModelLocation(GeoRenderState renderState) {
+        return ModState.resourcePackLoaded ? bossModel : fallbackModel;
     }
 
     @Override
-    public Identifier getTextureResource(GeoRenderState renderState) {
-        return buildFormattedTexturePath(selectedAsset());
+    public Identifier getTextureLocation(GeoRenderState renderState) {
+        return ModState.resourcePackLoaded ? bossTexture : fallbackTexture;
     }
 
     @Override
     public Identifier getAnimationResource(T animatable) {
-        return buildFormattedAnimationPath(selectedAsset());
+        return ModState.resourcePackLoaded ? bossAnimation : fallbackAnimation;
     }
 
-    private Identifier selectedAsset() {
-        return ModState.resourcePackLoaded ? bossAsset : FALLBACK_ASSET;
+    private static Identifier asset(String path) {
+        return Identifier.fromNamespaceAndPath(GuardianMod.MOD_ID, path);
     }
 }
