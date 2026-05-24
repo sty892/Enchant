9b0d777b2dbb82fd290fa9be65a5e2296023267e
styy_ <le4.tka@yandex.ru>
2026-05-24 21:18:41 +0200

    Require external boss resource pack assets

--- client/src/main/java/me/guardian/client/GuardianResourcePackAssets.java
+++ client/src/main/java/me/guardian/client/GuardianResourcePackAssets.java
@@ -1,13 +1,26 @@
 package me.guardian.client;
 
+import me.guardian.GuardianMod;
 import net.minecraft.resources.Identifier;
+import net.minecraft.server.packs.resources.Resource;
 import net.minecraft.server.packs.resources.ResourceManager;
 
 public final class GuardianResourcePackAssets {
+    private static final String MOD_PACK_ID = "mod/" + GuardianMod.MOD_ID;
+
     private GuardianResourcePackAssets() {
     }
 
     public static boolean hasExternalResource(ResourceManager manager, Identifier asset) {
-        return manager.getResource(asset).isPresent();
+        for (Resource resource : manager.getResourceStack(asset)) {
+            if (!isModResource(resource.sourcePackId())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isModResource(String sourcePackId) {
+        return GuardianMod.MOD_ID.equals(sourcePackId) || MOD_PACK_ID.equals(sourcePackId);
     }
 }
